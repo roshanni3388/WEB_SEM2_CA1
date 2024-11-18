80d5e9173014e9e0de3e57f59ddcbc20571cc0d0
roshani naik <roshanni3388@gmail.com>
2024-11-18 16:08:51 +0000

    re adding adminmodel.js and usermodel.js backend code

--- Airline Booking System/backend/models/AdminModel.js
+++ Airline Booking System/backend/models/AdminModel.js
@@ -1,10 +1,11 @@
 const mongoose= require('mongoose')
 
+
 const AdminSchema=mongoose.Schema({
     username:{type:String,required:true},
     password:{type:String,required:true},
     name:{type:String}
-})  
+})
 
 const AdminModel=mongoose.model('Admin',AdminSchema);
 module.exports=AdminModel;
