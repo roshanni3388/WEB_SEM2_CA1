1ce12a0423c1446b6afa3c94c7f1e309c53216e2
pvishnu04 <pvenkatavishnu2001@gmail.com>
2024-11-18 16:56:44 +0000

    Create AddDestination.js

---
+++ Airline Booking System/frontend/src/Mycomponent/AdminPages/AddDestination.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+import { Box, Button, Radio, RadioGroup, Stack } from '@chakra-ui/react'
+import axios from 'axios';
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 700px;
+  width: 100%;
+  background-color: #fff;
+  padding: 25px 30px;
+  border-radius: 5px;
+  box-shadow: 0 5px 10px rgba(0,0,0,0.15);
+`;
