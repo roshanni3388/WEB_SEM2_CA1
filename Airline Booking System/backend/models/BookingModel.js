2e0ead243a2f6a0e70a9eadd5d5e02ec067263ad
roshani naik <roshanni3388@gmail.com>
2024-11-20 12:42:32 +0000

    adding backend model

--- Airline Booking System/backend/models/BookingModel.js
+++ Airline Booking System/backend/models/BookingModel.js
@@ -7,3 +7,21 @@ const PassengerSchema = mongoose.Schema({
     gender: { type: String, required: true },
     bookingType: { type: String, required: true }
 });
+
+const BookingSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    flightId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Flight"
+    },
+    bookingId: { type: String, required: true },
+    date: { type: Date ,default:Date.now},
+    total: { type: String },
+    passengers: [PassengerSchema] // Array of passengers
+});
+
+const BookingModel = mongoose.model('Booking', BookingSchema);
+module.exports = BookingModel;
