d70a151b4f714bf925da6e885314c563ed0254ed
pvishnu04 <pvenkatavishnu2001@gmail.com>
2024-11-26 16:30:05 +0000

    create AdminChangePassword.js

---
+++ Airline Booking System/frontend/src/Mycomponent/AdminPages/AdminChangePassword.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const Container = styled.div`
+  max-width: 700px;
+  width: 100%;
+  background-color: #fff;
+  padding: 25px 30px;
+  border-radius: 5px;
+  box-shadow: 0 5px 10px rgba(0,0,0,0.15);
+`;
+
+const Title = styled.div`
+  font-size: 25px;
+  font-weight: 500;
+  position: relative;
+
+  ::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    height: 3px;
+    width: 30px;
+    border-radius: 5px;
+    background: linear-gradient(135deg, #71b7e6, #9b59b6);
+  }
+`;
