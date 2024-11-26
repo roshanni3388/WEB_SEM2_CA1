3f0b7c5a6ef5f5e969c036da8b6637599255c22d
roshani naik <roshanni3388@gmail.com>
2024-11-26 09:31:17 +0000

    userupdateprofile.js

---
+++ Airline Booking System/frontend/src/Mycomponent/UserPages/UserUpdateProfile.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+import { Box, Button, Center, Image, Radio, RadioGroup, Stack } from '@chakra-ui/react'
+import axios from 'axios';
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 700px;
+  width: 100%;
+  background-color: #fff;
+  padding: 25px 30px;
+  border-radius: 5px;
+  box-shadow: 0 5px 10px rgba(0,0,0,0.15);
+`;
+
+const Title = styled.div`
+  font-size: 25px;
+  font-weight: 500;
+  position: relative;
+
+  ::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    height: 3px;
+    width: 30px;
+    border-radius: 5px;
+    background: linear-gradient(135deg, #71b7e6, #9b59b6);
+  }
+`;
+
+const UserDetails = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 20px 0 12px 0;
+`;
+
+const InputBox = styled.div`
+  margin-bottom: 15px;
+  width: calc(100% / 2 - 20px);
+`;
+
+const Input = styled.input`
+  height: 45px;
+  width: 100%;
+  outline: none;
+  font-size: 16px;
+  border-radius: 5px;
+  padding-left: 15px;
+  border: 1px solid #ccc;
+  border-bottom-width: 2px;
+  transition: all 0.3s ease;
+
+  &:focus, &:valid {
+    border-color: #9b59b6;
+  }
+`;
+
+const GenderDetails = styled.div`
+  font-size: 20px;
+  font-weight: 500;
+`;
+
+const Category = styled.div`
+  display: flex;
+  width: 80%;
+  margin: 14px 0;
+  justify-content: space-between;
+`;
