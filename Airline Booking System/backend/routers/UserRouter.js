7808474271a9a32554339d5fbecee9ac7f77537e
roshani naik <roshanni3388@gmail.com>
2024-12-05 15:50:24 +0000

    modifying api router  userrouter.js

--- Airline Booking System/backend/routers/UserRouter.js
+++ Airline Booking System/backend/routers/UserRouter.js
@@ -13,3 +13,49 @@ const UserModel = require('../models/UserModel');
 const FlightModel = require('../models/FlightModel');
 const BookingModel = require('../models/BookingModel');
 const ContactModel = require('../models/ContactModel');
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.static('public'));
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+    }));
+
+
+    const storage = multer.diskStorage({
+      destination: (req, file, cb) => {
+          cb(null, 'public/images')
+      },
+      filename: (req, file, cb) => {
+          cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
+      }
+    })
+    
+    const upload = multer({
+        storage: storage
+    })
+    const verifyuser= async (req,res,next)=>{
+        // console.log(req.cookies)
+        const token=req.cookies.Token;
+        // console.log(token)
+        if(!token){
+           return res.json({Error:"The token was not available"})
+        }else{
+         jwt.verify(token,"jwt-secret-key",(err,decoded)=>{
+        if(err){
+         console.error(err)
+         return res.json({Error:"Token is  wrong"})
+        }
+        // console.log(decoded);
+        req.role=decoded.role;
+        req.user=decoded.user
+        req.id=decoded.id
+        next()
+    
+       })
+    
+        }
+    }
+
