1b7350e3e660d304b083a9155fe6a33a99ae3609
roshani naik <roshanni3388@gmail.com>
2024-12-06 04:49:15 +0000

    modifuing App.js

--- Airline Booking System/frontend/src/App.js
+++ Airline Booking System/frontend/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Mycomponent/Pages/Home';
+import DataHome from './Mycomponent/Pages/DataHome';
+import ContactPage from './Mycomponent/Pages/ContactPage';
+import AdminLogin from './Mycomponent/AdminPages/AdminLogin';
+import AdminPanel from './Mycomponent/AdminPages/AdminPanel';
+import DashBoard from './Mycomponent/AdminPages/DashBoard';
+import Signup from './Mycomponent/UserPages/Signup';
+import Login2 from './Mycomponent/UserPages/Login2';
+import UserPanel from './Mycomponent/UserPages/UserPanel';
+import UserChangePassword from './Mycomponent/UserPages/UserChangePassword';
+import AdminChangePassword from './Mycomponent/AdminPages/AdminChangePassword';
+import UserMyProfile from './Mycomponent/UserPages/UserMyProfile';
+import UserUpdateProfile from './Mycomponent/UserPages/UserUpdateProfile';
+import ViewAllUser from './Mycomponent/AdminPages/ViewAllUser';
+import AddFlight from './Mycomponent/AdminPages/AddFlight';
+import ViewAllFlights from './Mycomponent/AdminPages/ViewAllFlights';
+import UpdateFlight from './Mycomponent/AdminPages/UpdateFlight';
+import UserDashBoard from './Mycomponent/UserPages/UserDashBoard';
+import ViewUserFlights from './Mycomponent/UserPages/ViewUserFlights';
+import ViewFlightDetails from './Mycomponent/UserPages/ViewFlightDetails';
+import BookFlights from './Mycomponent/UserPages/BookFlights';
+import ViewUserBookings from './Mycomponent/UserPages/ViewUserBookings';
+import ViewBookingDetails from './Mycomponent/UserPages/ViewBookingDetails';
+import ViewAllBooking from './Mycomponent/AdminPages/ViewAllBooking';
+import UserBookingDetails from './Mycomponent/AdminPages/UserBookingDetails';
+import PaymentPage from './Mycomponent/UserPages/PaymentPage';
+import ViewAllContactAndFeedback from './Mycomponent/AdminPages/ViewAllContactAndFeedback';
+import FrontContactPage from './Mycomponent/Pages/FrontContactPage';
+import FrontViewFlights from './Mycomponent/Pages/FrontViewFlights';
+import AddDestination from './Mycomponent/AdminPages/AddDestination';
+import ViewDestination from './Mycomponent/AdminPages/ViewDestination';
+import FrontDestinationPage from './Mycomponent/Pages/FrontDestinationPage';
+import UpdateDestination from './Mycomponent/AdminPages/UpdateDestination';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+
+
+
